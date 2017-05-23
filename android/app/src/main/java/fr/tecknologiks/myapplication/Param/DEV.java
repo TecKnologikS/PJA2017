b934e4796c840ad38a9695d8f0cebf066417d13f
MEHU Pierre <p.mehu@hotmail.fr>
2017-05-24 00:31:35 +0200

    Création des devis via DEV.java

---
+++ android/app/src/main/java/fr/tecknologiks/myapplication/Param/DEV.java
@@ -0,0 +1,36 @@
+package fr.tecknologiks.myapplication.Param;
+
+/**
+ * Created by pierre on 23/05/2017.
+ */
+
+public class DEV {
+    public static String URL = "http://commercial.tecknologiks.com/index.php";
+
+    public static String LOGIN = "/login?login={login}&mdp={mdp}";
+
+    public static String DEVIS = "/{id}/{token}/products?limit={limit}&start={start}";
+    public static String DEVI = "/{id}/{token}/products/{id_product}/";
+
+    public static String Login(String user, String mdp) {
+        return LOGIN.replace("{user}", user)
+                .replace("{mdp}", mdp);
+    }
+
+    public static String Devis(String id, String token) {
+        return Devis(id, token, 100, 0);
+    }
+
+    public static String Devis(String id, String token, int limit, int start) {
+        return DEVIS.replace("{id}", id)
+                        .replace("{token}", token)
+                        .replace("{limit}", String.valueOf(limit))
+                        .replace("{start}", String.valueOf(start));
+    }
+
+    public static String Devi(String id, String token, int id_dev) {
+        return DEVI.replace("{id}", id)
+                .replace("{token}", token)
+                .replace("{id_product}", String.valueOf(id_dev));
+    }
+}
