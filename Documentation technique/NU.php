182309f18a70b030c735d2c50bb2b934b02e83f2
MEHU Pierre <p.mehu@hotmail.fr>
2017-06-16 10:02:40 +0200

    Push NU.php

---
+++ Documentation technique/NU.php
@@ -0,0 +1,149 @@
+ 
+
+Projet Annuel - ESGI
+
+Notice utilisateur – 
+Nom de l’application]
+
+
+Pierre MEHU &
+ Robin PAUQUET – 
+ 5A AL1
+
+12/
+07/
+2017
+
+
+
+ Cette documentation, 
+ à destination de l’ut
+ ilisateur final, est u
+ tile pour connaître l’
+ application [nom de l
+ ’application]. Elle e
+ xplique quelles fonct
+ ions elle comprend et 
+ comment les utiliser.
+
+
+ 
+Contenu
+I.	Le site :	2
+1.	Connexion au site	2
+2.	Articles	2
+3.	Devis	2
+4.	Factures	3
+II.	L’application	3
+
+
+
+ 
+I.	Le site :
+1.	Connexion au site
+
+Le site est disponible
+ à cette adresse : 
+Lorsque vous vous conn
+ectez 
+au site à l’aide de vo
+s identifiants (adress
+e mail et mot de passe), 
+vous accédez à la page
+ suivante :
+
+[SCREEN ECRAN D’ACCEUIL]
+
+Sur cette page vous tr
+ouverez l’ensemble des
+ paramètres de votre c
+ ompte,
+ 
+ 
+vous pouvez également 
+accéder aux différents 
+modules de recherche et
+ consulter vos devis, 
+vos commandes, vos factures...
+
+2.	Articles
+
+En cliquant sur Articles, 
+vous accéderez à cette page :
+
+[Screen ARTICLE]
+
+Ici, se trouve la liste 
+des articles à vendre.
+
+3.	Devis
+
+Vous pouvez consulter,
+ modifier, valider et
+ supprimer vos devis su
+ r la page «Devis» acce
+ ssible depuis la page d’accueil :
+
+[SCREEN DEVIS]
+
+
+Vous avez a
+lors accès à la liste d
+e vos devis validés et qu
+atre options différentes v
+ous sont accessibles en cl
+iquant sur la référence de 
+votre devis. 
+Ce bouton vous permet si
+mplement de consulter vo
+s devis. Une copie du de
+vis en question s’affich
+e alors, vous avez la po
+ssibilité de l’imprimer. 
+Ce bouton vous permet de
+ modifier les quantités 
+ souhaitées. Saisissez l
+ a nouvelle quantité puis 
+ cliquer sur «MAJ». 
+Ce bouton vous permet de p
+asser l’intégralité de vo
+tre devis en commande. Ce 
+bouton vous permet de supp
+rimer un de vos devis. Par
+ sécurité, la LDE conserve
+ une trace de ces devis supp
+ rimés pendant quelques temps.
+
+
+Une fois réalisé, le devis e
+st généré. Un fichier .pdf 
+est généré. 
+Par défaut, ce devis est té
+léchargé en local (soit sur 
+le PC ou le mobile/tablette)
+. Il est également possible 
+de l’envoyer par email.
+4.	Factures
+
+
+Vous pouvez consulter et im
+primer vos factures ainsi 
+ue suivre les colis expédié
+s sur la page «Facture» acce
+ssible la page d’accueil. 
+
+
+Ainsi, si vous cliquez su
+r le bouton «Voir la fact
+ure», vous voyez apparaîtr
+e un duplicata de la factu
+re en question que vous pou
+vez imprimer.
+
+5.	GPS
+
+Un GPS permet au commerci
+al de se déplacer
+II.	L’application
+
+
