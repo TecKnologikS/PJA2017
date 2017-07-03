1c6cf1043562eff8d1e32caee1eb5c409b80b7da
PIERRÔT\Pierre <p.mehu@hotmail.fr>
2017-07-03 19:58:47 +0200

    MAJ projet

---
+++ Documentation technique/Projet.php
@@ -0,0 +1,39 @@
+Bonjour,
+
+Ici, se trouve la description de notre projet annuel:
+
+Il sera composé:
+•	D'un site WEB (PHP, JS)
+•	D'une application mobile iOS (SWIFT)
+•	D'une application mobile Androïd (Androïd)
+
+Contexte du projet:
+Une entreprise vend divers produits. Depuis plusieurs années, les commerciaux itinérants possèdent un catalogue au format papier comportant les produits qu’ils ont à vendre ainsi qu’un fichier Excel avec quelques macros leur permettant de réaliser des calculs afin de générer une proposition chiffrée. Une fois la proposition effectuée, le commercial doit établir un bon de commande manuscrit sur lequel il reporte les éléments générés par son tableur Excel. 
+
+Cette solution présente des inconvénients :
+•	Les commerciaux doivent se déplacer avec un PC pour pouvoir utiliser Excel facilement 
+•	Tous les OS ne sont pas compatibles avec Excel (Apple notamment).
+
+
+Notre solution : 
+
+Nous aidons cette entreprise en lui proposant une solution qui répond à ses besoins.
+Notre application a pour but de simplifier le quotidien des commerciaux ainsi que d’éviter des erreurs humaines (lors de la réécriture de la proposition par exemple)
+
+Le site web et les deux applications ont les mêmes buts et fonctionnalités, à savoir:
+
+Les commerciaux peuvent s’y connecter via un login et un mot de passe. 
+Ce site contient leur catalogue ou ils peuvent rechercher rapidement les produits qu’ils ont à vendre. Le site réalise ensuite une proposition chiffrée en fonction des différentes informations du client saisie par le commercial (nom, adresse, siret, téléphone, catégorie…). 
+Enfin, une fois la proposition générée, celle-ci est stockée sur un serveur (ou envoyée à l’adresse mail de la secrétaire qui traite les commandes) afin d’être en sécurité.
+
+En plus du besoin du client, pour un projet encore plus intéressant, nous décidons d'y ajouter : 
+•	Optimisation du temps de transport si visite de plusieurs clients.
+•	Calcul statistique pour la proposition de produits supplémentaires/complémentaires
+•	Calcul de la commission en fonction de la catégorie des ventes et du vendeur ainsi que des remises faites par celui ci.
+
+Autres idées en options :
+•	Prévision de vente / des commissions  ? 
+•	Analyse des ventes par rapports aux autres vendeurs et suggestions d'améliorations (pas assez de ventes dans certains domaines, pertes de commission en vendant un objet plutôt qu'un autre etc....)
+
+Bien cordialement,
+Pierre et Robin
