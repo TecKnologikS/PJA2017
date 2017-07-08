707e4773ae760ad17816bbc9681f9a72732ad14d
PIERRÔT\Pierre <p.mehu@hotmail.fr>
2017-07-09 01:00:11 +0200

    Requetes details

--- Base de données/convertTable12.sql
+++ Base de données/convertTable12.sql
@@ -833,3 +833,16 @@ INSERT INTO `article_details` (`ID_Article`, `W`, `K`, `Lm`, `LmW`, `F`, `V`, `I
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+//creer les categorie
+INSERT INTO categorie (name)
+SELECT DISTINCT `Code Fam` FROM `TABLE 12` WHERE 1
+
+// A PARTIR DE LA TABLE 12, on créer les articles
+INSERT INTO `article`(`ref`, `name`, `smallDesc`, `about`, `image`, `categorie`, `tags`, `prix`)
+SELECT t.`REFERENCE`, t.`LIBELLE court`, t.`LIBELLE long`, '', '', c.id , '', t.`PV UNITAIRE (HT)` FROM `TABLE 12` t INNER JOIN categorie c ON c.name = t.`Code Fam`  WHERE 1
+
+// CREER ENSUITE LES DETAILS D ARTICLES
+INSERT INTO `article_details`(`ID_Article`, `W`, `K`, `Lm`, `LmW`, `F`, `V`, `IRC`, `H`, `Longueur`, `Largeur`, `Profondeur`, `Poids`, `Min`, `Max`, `DEEE`)
+SELECT a.id, t.`PUISSANCE (W)`, t.`TEMPERATURE (K)`, t.`LUMINOSITE (Lm)`, t.`RATIO (Lm/W)`, t.`FAISCEAU (-°)`, t.`TENSION (V)`, t.`IRC`, t.`DUREE DE VIE (H)`, t.`Longueur`, t.`Largeur`, t.`Profondeur`, t.`Poids (g)`, t.`Minimum`, t.`Maximum`, t.`DEEE (HT) INCLUSE` FROM `TABLE 12` t INNER JOIN article a ON a.ref = t.`REFERENCE`
